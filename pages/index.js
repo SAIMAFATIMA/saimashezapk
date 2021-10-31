61fab49957c21835aa18a0fc6955707b03f89a41
SAIMAFATIMA <93372137+SAIMAFATIMA@users.noreply.github.com>
2021-10-31 12:38:40 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Heading, Page, Button } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+  <Button>create a template</Button>
+  <Button>edit the template</Button>
+  <Button>save the template</Button>
+  </Page>
+);
+
+export default Index;
